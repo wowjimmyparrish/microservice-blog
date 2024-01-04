9992fc69add79820cdb4d5bf3dbba69623a395a5
Jimmy <jparrish@citytelecoin.com>
2024-01-04 10:26:28 -0600

    update

--- event-bus/index.js
+++ event-bus/index.js
@@ -12,9 +12,11 @@ app.post("/events", (req, res) => {
 
   events.push(event);
 
-  axios.post("http://localhost:4000/events", event).catch((err) => {
+  axios.post("http://posts-clusterip-srv:4000/events", event).catch((err) => {
     console.log(err.message);
   });
+
+/*
   axios.post("http://localhost:4001/events", event).catch((err) => {
     console.log(err.message);
   });
@@ -24,12 +26,14 @@ app.post("/events", (req, res) => {
   axios.post("http://localhost:4003/events", event).catch((err) => {
     console.log(err.message);
   });
+*/
+
   res.send({ status: "OK" });
 });
 
 app.get("/events", (req, res) => {
   res.send(events);
-})
+});
 
 app.listen(4005, () => {
   console.log("Listening on 4005");
